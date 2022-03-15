5d5e0e914188c98e4819eaea76e5a132cb6b24e8
KristofferHaakonsen <kristofferhhaakonsen@gmail.com>
2022-03-15 23:07:24 +0100

    INIT

---
+++ index.js
@@ -0,0 +1,36 @@
+const core = require('@actions/core')
+const github = require('@actions/github')
+
+const main = async () => {
+  try {
+    // Get input variables
+    const owner = core.getInput('owner', { required: true })
+    const repo = core.getInput('repo', { required: true })
+    const pr_number = core.getInput('pr_number', { required: true })
+    const token = core.getInput('token', { required: true })
+
+    // https://octokit.github.io/rest.js/v18
+    const octokit = new github.getOctokit(token)
+
+    // Create the body of the comment
+    let bodyString = `Please answer the following ${questions.length} questions about this PR:\n`
+    questions.forEach(
+      (element, index) => (bodyString += `\n${index + 1}. ${element} ___`)
+    )
+    bodyString += `\n\n\n- [ ] I have answered the questions above`
+
+    const result = await octokit.rest.issues.createComment({
+      owner,
+      repo,
+      issue_number: pr_number,
+      body: bodyString,
+    })
+
+    core.setOutput('comment_id', result.data.id)
+  } catch (error) {
+    core.setFailed(error.message)
+  }
+}
+
+// Call the main function to run the action
+main()
